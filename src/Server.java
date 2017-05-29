95e9fc40d288bc6f5d108d5e66fc5afad111eb03
Nomdrya <raymond.bressoud@gmail.com>
2017-05-29 14:52:29 +0200

    Premier commit

---
+++ src/Server.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Created by raymo on 5/25/2017.
+ */
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ssock = new ServerSocket(9999);
+        Socket socket = ssock.accept();
+        Scanner s = new Scanner(System.in);
+        PrintWriter p = new PrintWriter(socket.getOutputStream());
+        BufferedReader r = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        while (true){
+            String msg = s.nextLine();
+            p.println(msg);
+            p.flush();
+            String msgrecu = r.readLine();
+            System.out.println(msgrecu);
+        }
+    }
+}
